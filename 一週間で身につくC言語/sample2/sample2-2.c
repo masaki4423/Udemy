3dcc200015255d1dda445f6c82a30b32472b7d07
masaki4423 <kashiwabara356@gmail.com>
2021-03-11 20:38:20 +0900

    add sample2

---
+++ 一週間で身につくC言語/sample2/sample2-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    int b = 3;
+    int add, sub;
+    double avg;
+    a = 6;
+    add = a + b;
+    sub = a - b;
+    avg = (a + b) / 2.0;
+    printf("%d + %d = %d\n", a, b, add);
+    printf("%d - %d = %d\n", a, b, sub);
+    printf("%dと%dの平均値：%f\n", a, b, avg);
+}
